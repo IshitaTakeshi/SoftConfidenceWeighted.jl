93903d065cd1ff8f3f0c715668f05c804c5561f9
sonicair <ishitah.takeshi@gmail.com>
2015-12-22 10:44:05 +0900

    Use timeit instead of cProfile

--- profile/linearsvc.py
+++ profile/linearsvc.py
@@ -1,15 +1,22 @@
-import cProfile
+import timeit
+
 from sklearn.svm import LinearSVC
 from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import accuracy_score
 
 
+setup = """
+from sklearn.svm import LinearSVC
+from sklearn.datasets import load_svmlight_file
 X, y = load_svmlight_file("data.txt")
-
 svc = LinearSVC()
+"""
 
-cProfile.runctx('svc.fit(X, y)', {'svc': svc, 'X': X, 'y': y}, {})
+time = timeit.timeit('svc.fit(X, y)', setup=setup, number=1)
+print("Time: {}".format(time))
 
+X, y = load_svmlight_file("data.txt")
+svc = LinearSVC()
 svc.fit(X, y)
 results = svc.predict(X)
 accuracy = accuracy_score(y, results)
